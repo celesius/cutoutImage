1dd6f832f5f9743e6e83ce7e7ce3d8a74a7e8571
leisheng526 <leisheng526@hotmail.com>
2014-10-31 17:59:47 +0800

    Initial Commit

---
+++ opencv_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv_test
+//
+//  Created by leisheng526 on 14/10/31.
+//  Copyright (c) 2014年 leisheng526. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
